4391d2b8046679f262b1da691080d055a6037b37
Jinho <dfghcvb11@naver.com>
2023-01-27 02:00:16 +0900

    Test: splitAndSum_null_또는_빈문자() 테스트 코드

--- src/test/java/calculator/StringParserTest.java
+++ src/test/java/calculator/StringParserTest.java
@@ -1,7 +1,8 @@
 package calculator;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 //@DisplayName("StringParserTest")
@@ -10,6 +11,15 @@ class StringParserTest {
     @Test
     void 문자열_양수_더하기_테스트() {
 
-        Assertions.assertEquals(StringParser.plusNumbersInString("1,2,3", ","), 6);
+        assertEquals(StringCalculator.splitAndSum("1,2,3"), 6);
+    }
+
+    @Test
+    public void splitAndSum_null_또는_빈문자() throws Exception {
+        int result = StringCalculator.splitAndSum(null);
+        assertEquals(result, 0);
+
+        result = StringCalculator.splitAndSum("");
+        assertEquals(result, 0);
     }
 }
