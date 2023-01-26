82894d437addc0eb44e5af6bd7e514d625270e21
Jinho <dfghcvb11@naver.com>
2023-01-27 02:41:27 +0900

    feat: splitAndSum() 입력 문자열에 커스텀 구분자를 지정할 수 있는 기능 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,15 +1,29 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public static int splitAndSum(String numberString) {
-        if (numberString == null || numberString.isEmpty()) {
+    public static int splitAndSum(String inputString) {
+        if (inputString == null || inputString.isEmpty()) {
             return 0;
         }
 
-        return Arrays.stream(numberString.split("[,:]"))
+        String delimiter = "[,:]";
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(inputString);
+        if (m.find()) {
+            delimiter = m.group(1);
+            String numberString = m.group(2);
+            return splitByDelimiterAndSum(numberString, delimiter);
+        }
+
+        return splitByDelimiterAndSum(inputString, delimiter);
+    }
+
+    public static int splitByDelimiterAndSum(String numberString, String delimiter) {
+        return Arrays.stream(numberString.split(delimiter))
             .map(String::trim)
             .mapToInt(Integer::parseInt)
             .sum();
